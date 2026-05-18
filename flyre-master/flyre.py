92d554dafeda62031feb7ba960294ac31b9cc726
Dương <trandaiduong.nguyen@gmail.com>
2026-05-18 13:33:19 +0700

    Add project files.

---
+++ flyre-master/flyre.py
@@ -0,0 +1,18 @@
+def main():
+
+    import sys
+
+    if sys.version_info < (3, 8):
+        print("Flyre needs python 3.8 or above to run.")
+        print("In case you have troubles to get the dependencies you can head to")
+        print("\thttps://cozyfractal.itch.io/flyre")
+        print("to find Linux and Windows executables.")
+        sys.exit(1)
+
+    from src.engine import SIZE, App, IntegerScaleScreen
+    from src.states.loading import LoadingState
+
+    App(LoadingState, IntegerScaleScreen(SIZE)).run()
+
+if __name__ == "__main__":
+    main()
